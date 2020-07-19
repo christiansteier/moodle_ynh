2afb0fee04d75e135091ad78b504e462211981a1
root <root@h2890451.stratoserver.net>
2020-07-19 08:55:54 +0200

    Version 3.7 based on MySQL as DB

---
+++ README.md
@@ -0,0 +1,33 @@
+# Moodle app for YunoHost
+[![Install Moodle with YunoHost](https://install-app.yunohost.org/install-with-yunohost.png)](https://install-app.yunohost.org/?app=Moodle)<br><br>
+
+Version: **3.7+**
+Moodle - the world's open source learning platform
+
+Moodle <https://moodle.org> is a learning platform designed to provide
+educators, administrators and learners with a single robust, secure and
+integrated system to create personalised learning environments.
+
+Moodle is widely used around the world by universities, schools, companies and
+all manner of organisations and individuals.
+
+# Installation
+** After the installation go to your domain https://domain.tld/moodle and create the admin account. **
+
+### Installing guide
+
+ 1. App can be installed by YunoHost **admin web-interface** or by **running following command**:
+
+         $ sudo yunohost app install https://github.com/christiansteier/moodle_ynh
+
+ 
+### Upgrade this package:
+
+        $ sudo yunohost app upgrade moodle -u https://github.com/christiansteier/moodle_ynh
+
+## To-do
+- [X] Install script
+- [X] Remove script
+- [ ] Upgrade script
+- [X] Backup and Restore scripts(need testing)
+- [X] LDAP integration (silent installation only!)
---
+++ scripts/_common.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+PHPVERSION=7.2
+pkg_dependencies="php$PHPVERSION-zip php$PHPVERSION-mysql php$PHPVERSION-xml php$PHPVERSION-intl php$PHPVERSION-mbstring php$PHPVERSION-gd php$PHPVERSION-curl php$PHPVERSION-soap php$PHPVERSION-xmlrpc php$PHPVERSION-ldap"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+MOODLE_VERSION=3
+MOODLE_RELEASE=7
+
+ynh_install_composer() {
+	EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+	php$PHPVERSION -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	ACTUAL_SIGNATURE="$(php$PHPVERSION -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+	if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+		>&2 ynh_print_info "ERROR: Invalid installer signature for composer"
+		rm composer-setup.php
+		exit 1
+	fi
+	php$PHPVERSION composer-setup.php  --install-dir=/usr/local/bin --filename=composer --quiet
+	RESULT=$?
+	rm composer-setup.php
+}
+
+ynh_install_moosh() {
+	ynh_install_composer
+	if [ ! -d /opt/moosh ]; then
+		ynh_print_info "Installing MOOSH..."
+		if [ ! -d /opt ]; then mkdir /opt ; fi
+		git clone git://github.com/tmuras/moosh.git /opt/moosh 
+		cd /opt/moosh
+		composer install
+		ln -s /opt/moosh/moosh.php /bin/moosh
+	fi
+}
+
+ynh_install_moodle() {
+        ynh_print_info "Downloading Moodle..."
+        local MOODLE_APP="https://download.moodle.org/download.php/direct/stable${MOODLE_VERSION}${MOODLE_RELEASE}/moodle-latest-${MOODLE_VERSION}${MOODLE_RELEASE}.tgz"
+        local MOODLE_SHA256=${MOODLE_APP}.sha256
+        curl -OL ${MOODLE_APP}
+        curl -OL ${MOODLE_SHA256}
+        ynh_print_info "Verifying integrity..."
+        CHECKSUM_STATE=$(echo -n $(sha256sum -c moodle-latest-${MOODLE_VERSION}${MOODLE_RELEASE}.tgz.sha256) | tail -c 2)
+        if [ "${CHECKSUM_STATE}" != "OK" ]; then  ynh_print_info "[!!] Checksum does not match!" && exit 1; fi
+        ynh_print_info "All seems good, now unpacking moodle-latest-${MOODLE_VERSION}${MOODLE_RELEASE}.tgz"
+	tar zxvf moodle-latest-${MOODLE_VERSION}${MOODLE_RELEASE}.tgz 
+	mv moodle $final_path
+        chown -R $app:$app $final_path
+        chmod -R 755 $final_path
+        rm moodle-latest-${MOODLE_VERSION}${MOODLE_RELEASE}.*
+}
+
+ynh_install_moodle_language() {
+	if [ $language = "de" ]; then
+		curl -LO https://download.moodle.org/download.php/direct/langpack/${MOODLE_VERSION}.${MOODLE_RELEASE}/de.zip
+		unzip de.zip -d $var_root/lang/
+		rm de.zip
+	fi
+	if [ $language = "fr" ]; then
+	        curl -LO https://download.moodle.org/download.php/direct/langpack/${MOODLE_VERSION}.${MOODLE_RELEASE}/fr.zip
+                unzip fr.zip -d $var_root/lang/
+                rm fr.zip
+        fi
+}
+
+ynh_install_moodle_ldap() {
+	mysql <<-EOF
+USE ${db_name};
+UPDATE mdl_config_plugins SET value = 'localhost' WHERE plugin = 'auth_ldap' AND name = 'host_url';
+UPDATE mdl_config_plugins SET value = 'ou=users,dc=yunohost,dc=org' WHERE plugin = 'auth_ldap' AND name = 'contexts';
+UPDATE mdl_config_plugins SET value = 'uid' WHERE plugin = 'auth_ldap' AND name = 'user_attribute';
+UPDATE mdl_config_plugins SET value = '(objectClass=mailAccount)'WHERE plugin = 'auth_ldap' AND name = 'objectclass';
+UPDATE mdl_config_plugins SET value = 'givenName' WHERE plugin = 'auth_ldap' AND name = 'field_map_firstname';
+UPDATE mdl_config_plugins SET value = 'sn' WHERE plugin = 'auth_ldap' AND name = 'field_map_lastname';
+UPDATE mdl_config_plugins SET value = 'mail' WHERE plugin = 'auth_ldap' AND name = 'field_map_email';
+UPDATE mdl_config SET value = 'email,ldap' WHERE name = 'auth';
+EOF
+
+
+ldapsearch -x -LLL uid=* | grep uid: | cut -d: -f2 | sed "s* **g" > accounts
+while read LINE; do
+	cd $final_path
+	sudo -u $app moosh user-create $LINE
+	sudo -u $app moosh user-mod -a ldap $LINE
+done < accounts
+
+}
+
+# ============= FUTURE YUNOHOST HELPER =============
+# Delete a file checksum from the app settings
+#
+# $app should be defined when calling this helper
+#
+# usage: ynh_remove_file_checksum file
+# | arg: file - The file for which the checksum will be deleted
+ynh_delete_file_checksum () {
+	local checksum_setting_name=checksum_${1//[\/ ]/_}	# Replace all '/' and ' ' by '_'
+	ynh_app_setting_delete $app $checksum_setting_name
+}
+
+# Send an email to inform the administrator
+#
+# usage: ynh_send_readme_to_admin app_message [recipients]
+# | arg: app_message - The message to send to the administrator.
+# | arg: recipients - The recipients of this email. Use spaces to separate multiples recipients. - default: root
+#	example: "root admin@domain"
+#	If you give the name of a YunoHost user, ynh_send_readme_to_admin will find its email adress for you
+#	example: "root admin@domain user1 user2"
+ynh_send_readme_to_admin() {
+	local app_message="${1:-...No specific information...}"
+	local recipients="${2:-root}"
+
+	# Retrieve the email of users
+	find_mails () {
+		local list_mails="$1"
+		local mail
+		local recipients=" "
+		# Read each mail in argument
+		for mail in $list_mails
+		do
+			# Keep root or a real email address as it is
+			if [ "$mail" = "root" ] || echo "$mail" | grep --quiet "@"
+			then
+				recipients="$recipients $mail"
+			else
+				# But replace an user name without a domain after by its email
+				if mail=$(ynh_user_get_info "$mail" "mail" 2> /dev/null)
+				then
+					recipients="$recipients $mail"
+				fi
+			fi
+		done
+		echo "$recipients"
+	}
+	recipients=$(find_mails "$recipients")
+
+	local mail_subject="☁️🆈🅽🅷☁️: \`$app\` has important message for you"
+
+	local mail_message="This is an automated message from your beloved YunoHost server.
+Specific information for the application $app.
+$app_message
+---
+Automatic diagnosis data from YunoHost
+$(yunohost tools diagnosis | grep -B 100 "services:" | sed '/services:/d')"
+	
+	# Define binary to use for mail command
+	if [ -e /usr/bin/bsd-mailx ]
+	then
+		local mail_bin=/usr/bin/bsd-mailx
+	else
+		local mail_bin=/usr/bin/mail.mailutils
+	fi
+
+	# Send the email to the recipients
+	echo "$mail_message" | $mail_bin -a "Content-Type: text/plain; charset=UTF-8" -s "$mail_subject" "$recipients"
+}
